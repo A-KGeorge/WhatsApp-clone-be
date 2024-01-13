2e32eaa1ed2ef811cced669e2e27383fe4342bbd
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-13 13:18:41 -0330

    uncaught exception and httpError handling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,39 @@ import logger from "./configs/logger.config.js";
 //env variables
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  logger.error(`server is listening at ${PORT}...`);
+let server;
+
+server = app.listen(PORT, () => {
+  logger.info(`server is listening at ${PORT}...`);
+  logger.info(`pid is ${process.pid}`);
+});
+
+//handle server errors
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  if (error && error.message) {
+    logger.error(error.message);
+  } else {
+    logger.error("An unknown error occurred");
+  }
+  exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
