9a30c7429e57e297557248c913836b58dfb111fc
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-15 23:33:13 -0330

    sendMessage backend logic implemented

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,38 @@
+import logger from "../configs/logger.config.js";
+import {
+  createMessage,
+  populateMessage,
+  updateLatestMessage,
+} from "../services/message.service.js";
+
+export const sendMessage = async (req, res, next) => {
+  try {
+    const user_id = req.user.userId;
+    console.log(user_id);
+    const { message, convo_id, files } = req.body;
+    if (!convo_id || (!message && !files)) {
+      logger.error("Please provide a conversation id and a message body.");
+      return res.sendStatus(400);
+    }
+    const msgData = {
+      sender: user_id,
+      message,
+      conversation: convo_id,
+      files: files || [],
+    };
+    let newMessage = await createMessage(msgData);
+    let populatedMessage = await populateMessage(newMessage._id);
+    await updateLatestMessage(convo_id, newMessage);
+    res.json(populatedMessage);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    res.send("get messages");
+  } catch (error) {
+    next(error);
+  }
+};
