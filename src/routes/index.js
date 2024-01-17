edc6e4444fbd1b23f0aca1ac9953e557f926ae72
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-16 21:56:00 -0330

    search users backend logic implemented

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
-import authRoutes from "./auth.route.js";
-import conversationRoutes from "./conversation.route.js";
-import messageRoutes from "./message.route.js";
+import AuthRoutes from "./auth.route.js";
+import ConversationRoutes from "./conversation.route.js";
+import MessageRoutes from "./message.route.js";
+import UserRoutes from "./user.route.js";
 
 const router = express.Router();
 
-router.use("/auth", authRoutes);
-router.use("/conversation", conversationRoutes);
-router.use("/message", messageRoutes);
+router.use("/auth", AuthRoutes);
+router.use("/user", UserRoutes);
+router.use("/conversation", ConversationRoutes);
+router.use("/message", MessageRoutes);
 
 export default router;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,3 +6,13 @@ export const findUser = async (userId) => {
   if (!user) throw createHttpError("Please fill all fields.");
   return user;
 };
+
+export const searchUser = async (keyword) => {
+  const users = await UserModel.find({
+    $or: [
+      { name: { $regex: keyword, $options: "i" } },
+      { email: { $regex: keyword, $options: "i" } },
+    ],
+  });
+  return users;
+};
